ace717718cde4534d7caa59c887aeaaca22f58e5
Marketa Sauerova <market.sauer@gmail.com>
2021-10-05 11:11:44 +0200

    Code fixes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const Apify = require('apify');
 
 const { utils: { log } } = Apify;
 
-exports.handleList = async ({ request, page }, requestQueue) => {
+exports.handleList = async ({ request, page }, requestQueue, maxResultsPerPage) => {
     // Wait for the network to settle, initially there will be 36 videos loaded. There
     // are more with a scroll event - not implemented yet.
     await page.waitForNetworkIdle({
@@ -10,13 +10,11 @@ exports.handleList = async ({ request, page }, requestQueue) => {
     });
 
     // video-feed list
-    let videoUrls = await page.evaluate(() => {
-        const result = [];
-        [...document.querySelectorAll('main .video-feed-item')].map((video) => {
-            result.push(video.querySelector('a')?.getAttribute('href'));
-        });
-        return result;
-    });
+    let videoUrls = await page.$$eval('main .video-feed-item', (els) => els.reduce((total, video) => {
+           total.push(video.querySelector('a')?.getAttribute('href'));
+           return total;
+    }, []).filter((videoUrl) => videoUrl));
+
     log.info(`[SEARCH VIDEOS]: Found ${videoUrls.length} videos.`)
     if (maxResultsPerPage !== undefined && maxResultsPerPage !== 0) {
         videoUrls = videoUrls.splice(0, maxResultsPerPage);
@@ -28,26 +26,33 @@ exports.handleList = async ({ request, page }, requestQueue) => {
         const header = await page.evaluate(() => {
             return {
                 searchHashtag: document.querySelector('header .share-title')
-                    .innerText
-                    .substr(1),
+                    ?.innerText
+                    .substr(1) || null,
                 numberOfViewsOnHashtag: document.querySelector('header [title="views"]')
-                    .innerText
-                    .split(' ')[0],
+                    ?.innerText
+                    .split(' ')[0] || null,
             };
         });
         for (const videoUrl of videoUrls) {
-            const userUrl = videoUrl.match(/.*\/video/)[0] || null
-            if (userUrl) {
-                await requestQueue.addRequest({
-                    url: userUrl.substr(0, (userUrl.length - 6)),
-                    userData: {
-                        label: 'USER',
-                        header,
-                        videoUrl,
-                    },
-                    uniqueKey: videoUrl.match(/[0-9]+/)[0],
-                });
-            }else{
+            const matchUrl = videoUrl.match(/.*\/video/);
+            const matchVideoId = videoUrl.match(/[0-9]+/);
+            if (matchUrl) {
+                const userUrl = matchUrl[0];
+                if (matchVideoId) {
+                    await requestQueue.addRequest({
+                        // '/video' part of the url we matched before have to be truncated
+                        url: userUrl.substr(0, (userUrl.length - 6)),
+                        userData: {
+                            label: 'USER',
+                            header,
+                            videoUrl,
+                        },
+                        uniqueKey:matchVideoId[0],
+                    });
+                } else {
+                    throw new Error ('The video has no id defined.');
+                }
+            } else {
                 throw new Error('User url was not found in video url.');
             }
         }
@@ -81,7 +86,7 @@ exports.handleUser = async ({ request, page }, requestQueue) => {
 };
 
 exports.handleVideo = async ({ request, page }) => {
-    let output = await page.evaluate((url, userInfo) => {
+    const output = await page.evaluate(() => {
         const nicknameAndDate = document.querySelector('.feed-item-content .author-nickname').innerText.split(' · ');
         const hashtags = [];
         [...document.querySelector('.feed-item-content .tt-video-meta-caption').querySelectorAll('a')]
@@ -94,7 +99,6 @@ exports.handleVideo = async ({ request, page }) => {
         const video = document.querySelector('.feed-item-content .item-video-container video');
 
         return {
-            ...userInfo,
             userImageUrl: document.querySelector('.video-detail .user-avatar img').getAttribute('src'),
             userName: document.querySelector('.feed-item-content .author-uniqueId').innerText,
             userNickname: nicknameAndDate[0] || null,
@@ -102,8 +106,6 @@ exports.handleVideo = async ({ request, page }) => {
             caption: document.querySelector('.feed-item-content .tt-video-meta-caption').querySelector('strong').innerText.trim(),
             hashtags,
             music: document.querySelector('.feed-item-content .tt-video-music').innerText,
-            videoUrlOnTiktok: url,
-            videoId: url.match(/[0-9]+/)[0],
             userId: video.getAttribute('authorid'),
             videoUrl: video.getAttribute('src'),
             likes: document.querySelector('.feed-item-content .item-video-container [title="like"]').innerText,
@@ -111,21 +113,19 @@ exports.handleVideo = async ({ request, page }) => {
             shares: document.querySelector('.feed-item-content .item-video-container [title="share"]').innerText,
             scrapedAt: new Date().toISOString(),
         };
-    }, request.url, request.userData.userInfo);
+    });
 
-    //Add header if it was a tag search
-    if (request.userData.header){
-        output = {
-            ...request.userData.header,
-            ...output,
-        }
-    }
-
-    await Apify.pushData(output);
+    await Apify.pushData({
+        ...(request.userData.header ?? {}),
+        ...request.userData.userInfo,
+        ...output,
+        videoUrlOnTiktok: request.url,
+        videoId: request.url.match(/[0-9]+/)[0],
+    });
 };
 
 const getUserInfo = async (page, url) => {
-    return await page.evaluate((url) => {
+    return page.evaluate((url) => {
         const shareLinks = [];
         if (document.querySelector('.share-links')) {
             [...document.querySelector('.share-links')
